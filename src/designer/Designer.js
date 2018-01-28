166eec353afeecb73865406b561d7704941c1787
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-01-28 09:54:21 +0100

    WIP Designer

--- src/designer/Designer.js
+++ src/designer/Designer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import reduce from 'lodash/reduce'
+import pick from 'lodash/pick'
+// import generateName from 'sillyname'
 
 const translations = {
   strength: 'Strength',
@@ -7,7 +10,17 @@ const translations = {
   speed: 'Speed'
 }
 
-const translate = (key) => translations[key]
+const translate = key => translations[key]
+
+const buildFields = (options, onChange) =>
+  reduce(
+    options,
+    (acc, value, name) => ({
+      ...acc,
+      [name]: { name, value, onChange: onChange(name) }
+    }),
+    {}
+  )
 
 const Skill = ({ name, value, onChange }) => (
   <fieldset>
@@ -23,6 +36,13 @@ const Skill = ({ name, value, onChange }) => (
   </fieldset>
 )
 
+const Text = ({ name, value, onChange }) => (
+  <fieldset>
+    <label>{translate(name)}</label>
+    <input type='text' name={name} value={value} onChange={onChange} />
+  </fieldset>
+)
+
 class Designer extends React.Component {
   constructor (props) {
     super(props)
@@ -30,35 +50,23 @@ class Designer extends React.Component {
     this.state = {
       strength: 3,
       defense: 2,
-      speed: 1
+      speed: 1,
+      name: 'hej'
     }
   }
 
   render () {
-    // const { skills } = this.props
     const onChange = field => event => {
-      this.setState({ [field]: event.target.value })
+      this.setState({ [field]: Number(event.target.value) })
     }
-    const fields = {
-      strength: {
-        name: 'strength',
-        value: this.state.strength,
-        onChange: onChange('strength')
-      },
-      defense: {
-        name: 'defense',
-        value: this.state.defense,
-        onChange: onChange('defense')
-      },
-      speed: {
-        name: 'speed',
-        value: this.state.speed,
-        onChange: onChange('speed')
-      }
-    }
+    const fields = buildFields(
+      pick(this.state, ['strength', 'defense', 'speed']),
+      onChange
+    )
     return (
       <div className='designer'>
         <form>
+          <Text {...fields.name} />
           <Skill {...fields.strength} />
           <Skill {...fields.defense} />
           <Skill {...fields.speed} />
