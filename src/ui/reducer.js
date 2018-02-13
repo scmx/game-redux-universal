c56b8dfa0548b2ad3df8a902ca7b469a0e67d3bc
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-02-13 21:21:11 +0100

    Fix typo use uiStates in reducer

--- src/ui/reducer.js
+++ src/ui/reducer.js
@@ -12,13 +12,13 @@ export default function uiReducer (state = uiStates.NICKNAME, action) {
       return uiStates.HALL_OF_FAME
     }
     case uiActions.NEW_GAME: {
-      return uiActions.NEW_GAME
+      return uiStates.NEW_GAME
     }
     case uiActions.HERO_MENU: {
-      return uiActions.HERO_MENU
+      return uiStates.HERO_MENU
     }
     case uiActions.HALL_OF_FAME: {
-      return uiActions.HALL_OF_FAME
+      return uiStates.HALL_OF_FAME
     }
     default: {
       return state
