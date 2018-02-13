63221e143a5467128aa6ba2eededa2eaf0765c1a
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-02-13 20:09:48 +0100

    Oops, add MenuOnlineOffline component

---
+++ src/menu/MenuOnlineOffline.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { uiCreators } from '../ui'
+
+class MenuOnlineOffline extends React.Component {
+  render () {
+    return (
+      <ul className='menu'>
+        <li>
+          <button onClick={this.props.playOnline}>Play online</button>
+        </li>
+        <li>
+          <button onClick={this.props.playOnline}>Play offline</button>
+        </li>
+      </ul>
+    )
+  }
+}
+
+export default connect(null, {
+  playOnline: uiCreators.playOnline,
+  playOffline: uiCreators.playOffline
+})(MenuOnlineOffline)
