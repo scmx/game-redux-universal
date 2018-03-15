419b154064422ed4b78cb2484658852fda135a0d
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-03-16 00:44:48 +0100

    Clean up HallOfFame

--- src/hallOfFame/HallOfFame.js
+++ src/hallOfFame/HallOfFame.js
@@ -1,26 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import map from 'lodash/map'
-import { characterCreators } from '../characters'
+import pick from 'lodash/pick'
 
 class HallOfFame extends React.Component {
-  componentDidMount () {
-    // const noCharacters = Object.keys(this.props.characters).length === 0
-    // if (noCharacters) {
-    //   this.props.designCharacter()
-    // }
-  }
-
   render () {
     return (
       <div className='hof'>
         Hall Of Fame
-        <button onClick={this.props.designCharacter}>New character</button>
+        <button>New character</button>
         <ul className='hof__character_list'>
-          {map(this.props.characters, character => (
-            <li key={character.id} className='hof__character'>
-              <button onClick={() => this.props.deriveCharacter(character)}>
-                {character.name}
+          {map(this.props.heroes, hero => (
+            <li key={hero.id} className='hof__hero'>
+              <button>
+                {hero.name}
               </button>
             </li>
           ))}
@@ -32,10 +25,7 @@ class HallOfFame extends React.Component {
 
 export default connect(
   state => ({
-    characters: state.characters
-  }),
-  {
-    designCharacter: characterCreators.designCharacter,
-    deriveCharacter: characterCreators.deriveCharacter
-  }
+    // TODO: List previous heroes instead
+    heroes: pick(state.heroChoices, state.heroChoicesByPlayer)
+  })
 )(HallOfFame)
