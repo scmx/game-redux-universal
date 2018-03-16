61f21f1c6c0701d81d45045d862163b300ec74f9
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-03-16 01:19:03 +0100

    Add randomHeroes and hero.id

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -6,6 +6,7 @@ import uuidv4 from 'uuid/v4'
 const randomStat = () => 0.1 + Math.random() * 0.8
 
 const randomHero = () => ({
+  id: uuidv4(),
   name: getSillyName(),
   level: 1,
   job: 'Mage',
@@ -18,6 +19,11 @@ const randomHero = () => ({
   }
 })
 
+const randomHeroes = count => Array(count)
+  .fill()
+  .map(randomHero)
+  .reduce((acc, hero) => ({ ...acc, [hero.id]: hero }), {})
+
 export default function serverMiddleware () {
   return store => {
     return next => action => {
@@ -27,11 +33,7 @@ export default function serverMiddleware () {
         case apiActions.HERO_CHOICES_LOAD_REQUEST: {
           const result = next(action)
 
-          store.dispatch(apiCreators.heroChoicesLoadSuccess({
-            [uuidv4()]: randomHero(),
-            [uuidv4()]: randomHero(),
-            [uuidv4()]: randomHero()
-          }))
+          store.dispatch(apiCreators.heroChoicesLoadSuccess(randomHeroes(3)))
 
           return result
         }
