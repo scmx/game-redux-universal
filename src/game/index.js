deacd031e611ed04044fafe7fb60ba6f9d56c872
Albert Arvidsson <albert.arvidsson@gmail.com>
2018-04-10 20:15:43 +0200

    Remove unused gameActions

--- src/game/index.js
+++
@@ -1,9 +0,0 @@
-import { buildConstant, stringArrayToStringObject } from '../utils'
-
-export const gameActions = buildConstant(stringArrayToStringObject('PLAY'))
-
-export const gameCreators = buildConstant({
-  play: () => ({
-    type: gameActions.PLAY
-  })
-})
